e9cbec1848b1f51ed3f7e9fc5da7e9a906aa3084
Jan Schulte <hello@unexpected-co.de>
2013-12-10 23:14:50 +0100

    Initial Commit

---
+++ promail/PMAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  PMAppDelegate.h
+//  promail
+//
+//  Created by Jan Schulte on 10/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ promail/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  promail
+//
+//  Created by Jan Schulte on 10/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
