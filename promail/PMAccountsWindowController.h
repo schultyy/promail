90fd3c5fee9db412d0e6f208bda33c0cb489ef7b
Jan Schulte <hello@unexpected-co.de>
2013-12-18 21:04:02 +0100

    Save accounts when the window gets closed

--- promail/PMAccountsWindowController.h
+++ promail/PMAccountsWindowController.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface PMAccountsWindowController : NSWindowController
+@interface PMAccountsWindowController : NSWindowController<NSWindowDelegate>
 
 @property (assign) NSManagedObjectContext *managedContext;
 
--- promail/PMAccountsWindowController.m
+++ promail/PMAccountsWindowController.m
@@ -30,4 +30,12 @@
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
+-(BOOL)windowShouldClose:(id)sender{
+    
+    NSError *error;
+    
+    [[self managedContext] save: &error];
+    return YES;
+}
+
 @end
