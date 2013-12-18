3bfce25285d401a5a5d8819f7dce5cc6d2ad360d
Jan Schulte <hello@unexpected-co.de>
2013-12-18 20:42:16 +0100

    Added Accounts Window and controller

---
+++ promail/PMAccountsWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PMAccountsWindowController.h
+//  promail
+//
+//  Created by Jan Schulte on 18/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PMAccountsWindowController : NSWindowController
+
+@end
---
+++ promail/PMAccountsWindowController.m
@@ -0,0 +1,33 @@
+//
+//  PMAccountsWindowController.m
+//  promail
+//
+//  Created by Jan Schulte on 18/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import "PMAccountsWindowController.h"
+
+@interface PMAccountsWindowController ()
+
+@end
+
+@implementation PMAccountsWindowController
+
+- (id)init
+{
+    self = [super initWithWindowNibName:@"AccountsWindow"];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
