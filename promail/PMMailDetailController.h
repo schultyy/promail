bfb07f404025c82bac758da6e8ede8c277c8da2b
Jan Schulte <hello@unexpected-co.de>
2013-12-24 23:19:31 +0100

    Added detail view

---
+++ promail/PMMailDetailController.h
@@ -0,0 +1,13 @@
+//
+//  PMMailDetailController.h
+//  promail
+//
+//  Created by Jan Schulte on 24/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PMMailDetailController : NSViewController
+
+@end
---
+++ promail/PMMailDetailController.m
@@ -0,0 +1,26 @@
+//
+//  PMMailDetailController.m
+//  promail
+//
+//  Created by Jan Schulte on 24/12/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import "PMMailDetailController.h"
+
+@interface PMMailDetailController ()
+
+@end
+
+@implementation PMMailDetailController
+
+- (id)init
+{
+    self = [super initWithNibName:@"PMMailDetailView" bundle:nil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
