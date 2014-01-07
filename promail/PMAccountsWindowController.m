92d5d693c28618103b3d9bd9515ecd8e47466f78
Jan Schulte <hello@unexpected-co.de>
2014-01-07 21:47:47 +0100

    white account window

--- promail/PMAccountsWindowController.m
+++ promail/PMAccountsWindowController.m
@@ -40,6 +40,7 @@
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     [[self window] setDelegate:self];
+    [[self window] setBackgroundColor: [NSColor whiteColor]];
 }
 
 -(NSArray *) accounts{
