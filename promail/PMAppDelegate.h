1d15207ab71288cbfa1eb370304988e1197bdb5d
Jan Schulte <hello@unexpected-co.de>
2014-02-28 19:24:17 +0100

    Can show Wizard view

--- promail/PMAppDelegate.h
+++ promail/PMAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 #import "PMMainController.h"
-#import "PMAccountsWindowController.h"
+#import "PMAccountWizardController.h"
 
 @interface PMAppDelegate : NSObject <NSApplicationDelegate>
 
@@ -17,7 +17,7 @@
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (retain) PMAccountsWindowController *accountsController;
+@property (retain) PMAccountWizardController *accountsController;
 @property (retain) PMMainController *mainController;
 
 - (IBAction)saveAction:(id)sender;
